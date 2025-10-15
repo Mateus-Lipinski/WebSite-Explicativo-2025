08163a7df2ad21d8beae8b20424846e05edb1b0b
Mateus-Lipinski <mateuslipinskimatias@gmail.com>
2025-10-15 13:47:56 -0300

    feat/create-components

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Footer() {
+
+    return (
+        <footer>
+            &copy; Todos os direitos reservados - Mateus Lipinski Matias
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Header() {
+    
+    return (
+        <header>
+            <h1>WebSite Explicativo</h1>
+        </header>
+    )
+}
+
+export default Header
